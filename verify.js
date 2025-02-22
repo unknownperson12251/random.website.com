10c85b7debd2496ddb6fd5354bdc398a21fabb9b
unknownperson12251 <hoang.angelic@gmail.com>
2025-02-22 16:52:46 +0800

    Add files via upload

---
+++ verify.js
@@ -0,0 +1,15 @@
+<script>
+    function fun() {
+        let inputPswd = document.getElementById("validate")
+                                .value;
+        let correctPswd = "240709";
+        if (inputPswd === correctPswd) {
+            window.location.href = "https://unknownperson12251.github.io/random.website.com/actualblog_yzx_938LTNO32837.html"
+        } else {
+            	document.getElementById("container")
+                	.style.display = "block";
+            	document.getElementById("pswd")
+                	.style.display = "none";
+        }
+    }
+</script>
