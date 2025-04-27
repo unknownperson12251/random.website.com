b9005708e4b8e8bc5ce5035a2df778b74edf482e
unknownperson12251 <hoang.angelic@gmail.com>
2025-04-27 22:25:13 +0800

    Update verify.js

--- verify.js
+++ verify.js
@@ -15,6 +15,8 @@ document.onkeydown = function(e) {
         if (e.ctrlKey && e.keyCode === 85 ) {
             return false;
         }
+};
+document.onkeydown = function(e) {
         if (e.keyCode === 13){
             fun();
         }
